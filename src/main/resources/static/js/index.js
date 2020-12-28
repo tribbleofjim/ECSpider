cd8a863bff470028934eef0ba707396fc14dc110
tribbleofjim <lyifee@qq.com>
2020-12-28 16:21:55 +0800

    handle index css

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -7,7 +7,15 @@ layui.use('layer', function(){
 
 function startSpider() {
     let keyword = document.getElementById('keyword').value;
+    if (keyword === null || keyword === '') {
+        layer.msg('请输入关键词！');
+        return;
+    }
     let threadNum = document.getElementById('threadNum').value;
+    if (threadNum === null || threadNum === '') {
+        layer.msg('请输入线程数！');
+        return;
+    }
     let xhr = new XMLHttpRequest();
     let url = "/spider?keyword=" + keyword + "&threadNum=" + threadNum;
     xhr.open('GET', url, true);

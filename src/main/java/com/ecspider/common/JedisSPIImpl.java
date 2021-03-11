288486e83cd3e186cc72122a484a6fe7f36a3966
tribbleofjim <lyifee@qq.com>
2021-03-11 13:21:05 +0800

    bug fix ~

--- src/main/java/com/ecspider/common/JedisSPIImpl.java
+++ src/main/java/com/ecspider/common/JedisSPIImpl.java
@@ -1,6 +1,7 @@
 package com.ecspider.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.webmaple.worker.config.JedisSPI;
 import redis.clients.jedis.JedisPool;
 
@@ -8,6 +9,7 @@ import redis.clients.jedis.JedisPool;
  * @author lyifee
  * on 2021/2/20
  */
+@Component
 public class JedisSPIImpl implements JedisSPI {
     @Autowired
     private JedisPool jedisPool;
